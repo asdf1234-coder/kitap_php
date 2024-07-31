34e5039a89e51b04668f18db52cd724fc0a5d3ed
asdf1234-coder <penspinning222@gmail.com>
2024-07-31 10:35:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>kitap html</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="style3.css" />
 </head>
 <body>
     <div class="header">
@@ -28,12 +28,14 @@
         <?php foreach($tum_kitaplar as $kitap): ?>
             <a href = "kitap_detay.php?id=<?php echo $kitap["id"]?>">
                 <div class="kitap">
-                    <div class="ust"><img src="img/<?php echo $kitap["resim"]?>" height="120px" width="100%"></div>
-                    <div class="alt"><?php echo $kitap["isim"] ?></div>
+                    <div class="ust"><img src="img/<?php echo $kitap["resim"]?>" height="100%" width="100%"></div>
+                    <div class="yazar_isim">
+                        <div class="alt yazar">yazar</div>
+                        <div class="alt"><?php echo $kitap["isim"] ?></div>
+                    </div>
                 </div>
             </a>
         <?php endforeach; ?>
     </div>
-    <a href="kitap_detay_hazirlik.php">detay</a>
 </body>
 </html>
